82297897b23f44751d00d1d5c9e920f6760de5d6
Jatin Singh <jatinsingh234@gmail.com>
2025-03-02 09:46:08 +0530

    fix s3

--- s3.py
+++ s3.py
@@ -23,6 +23,7 @@ class S3CRUD:
                 's3',
                 aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
                 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                aws_region_name='us-east-1'
             )
             self.bucket_name = bucket_name
         except Exception as e:
